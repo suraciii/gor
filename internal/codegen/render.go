52e0e4397633da8aad9f227a56698a0ee19eeeaa
Tequila Sunset <suraciii@outlook.com>
2026-08-05 21:35:47 +0800

    为生成的 Install 增加使用说明

--- internal/codegen/render.go
+++ internal/codegen/render.go
@@ -247,6 +247,10 @@ func {{$entity.ConstructorName}}(rt gor.Invoker, id gor.Identity) {{$.SourcePack
 }
 
 {{end}}
+// Install installs the generated entity bindings in rt.
+// Call it once after creating rt and before registering or referencing any of
+// the generated entity types. After it returns nil, gor.Register and gor.Ref
+// can use those types with rt.
 func Install(rt *gor.Runtime) error {
 {{range .Interfaces}}	if err := gor.InstallType[{{$.SourcePackage}}.{{.Name}}](rt, {{.DispatchName}}, {{.ConstructorName}}, new{{.Name}}Call); err != nil {
 		return err
